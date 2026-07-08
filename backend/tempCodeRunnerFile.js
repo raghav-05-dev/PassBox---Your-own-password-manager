0731be8ac57d1db85d23fa3fb37d67c30d68ea9f
raghav-05-dev <raghavp884@gmail.com>
2026-07-08 15:13:24 +0530

    Add files via upload

---
+++ backend/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+app.delete('/', async (req, res) => { 
+//     // const password = req.body
+//     const db = client.db(dbName);
+//     const collection = db.collection('passwords');
+//     const findResult = await collection.deleteOne(password);
+//     res.send(req.body)
+// })
